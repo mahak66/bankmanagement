575c4db72e7325e5ec13e010928b1e40328b6c6c
mahak66 <matanimahak@gmail.com>
2015-11-15 13:53:30 +0530

    Create header.c

---
+++ header.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void new_acc();
+void update(void);
+void transact(void);
+void customer_list();
+void details(void);
+void dlt(void);
+void exit_program();
+float interest(float t,float amount,int rate);
